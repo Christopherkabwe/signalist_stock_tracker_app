d5fc885f98942cca8d66d2b563e139e4ea1424ab
Christopher Kabwe <christopherkabwe97@gmail.com>
2025-11-25 10:15:16 +0200

    Fix homepage layout and add TradingView widget

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -42,15 +42,6 @@ const Home = () => {
                     />
                 </div>
             </section>
-            <div className="h-full w-full">
-                <TradingViewWidget
-                    title="Market Overview"
-                    scriptUrl={`${scriptUrl}market-overview.js`}
-                    config={MARKET_OVERVIEW_WIDGET_CONFIG}
-                    className="custom-chart"
-                    height={600}
-                />
-            </div>
         </div>
     )
 }
